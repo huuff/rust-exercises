418dccd91db24d6bfd1ed755710fed4c3b0f5c53
Haf <haf@protonmail.ch>
2023-12-23 11:03:33 +0100

    the fibs'

--- rust-book/ch3/fibonacci/src/main.rs
+++ rust-book/ch3/fibonacci/src/main.rs
@@ -1,5 +1,21 @@
+const ERROR_MESSAGE: &'static str = "You must provide a number by command line arguments";
+
 fn main() {
-    let n = &std::env::args().collect::<Vec<String>>().get(1);
+    let n = std::env::args().collect::<Vec<String>>().get(1)
+	.expect(ERROR_MESSAGE)
+	.parse::<u64>()
+	.expect(ERROR_MESSAGE)
+	;
     
-    println!("Hello, world!");
+    let result = recursive_fib(n);
+    
+    println!("{result}");
+}
+
+fn recursive_fib(n: u64) -> u64 {
+    if n <= 1 {
+	n
+    } else {
+	recursive_fib(n-1) + recursive_fib(n-2)
+    }
 }
