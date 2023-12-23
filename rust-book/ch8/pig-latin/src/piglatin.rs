fd333e6e110e4e006cfcfe46add3327b2ad588fb
Haf <haf@protonmail.ch>
2023-12-23 22:33:00 +0100

    vocal -> vowel

--- rust-book/ch8/pig-latin/src/piglatin.rs
+++ rust-book/ch8/pig-latin/src/piglatin.rs
@@ -4,7 +4,7 @@ pub fn piglatinize_word(word: &mut Vec<u8>) -> String {
     if word.is_empty() { return "".to_string() } 
 
     let first_letter = word[0];
-    if is_vocal(&first_letter) {
+    if is_vowel(&first_letter) {
 	format!("{}-hay", String::from_utf8_lossy(word))
     } else {
 	let first_letter = word.remove(0);
@@ -13,12 +13,12 @@ pub fn piglatinize_word(word: &mut Vec<u8>) -> String {
 }
 
 // TODO: Rename this to vowel lol
-const VOCALS: [u8; 10] = [
+const VOWELS: [u8; 10] = [
     b'a', b'e', b'i', b'o', b'u',
     b'A', b'E', b'I', b'O', b'U',
 ];
-fn is_vocal(byte: &u8) -> bool {
-    VOCALS.contains(&byte)
+fn is_vowel(byte: &u8) -> bool {
+    VOWELS.contains(&byte)
 }
 
 #[cfg(test)]
@@ -26,13 +26,13 @@ mod tests {
     use super::*;
     
     #[test]
-    fn detects_vocal() {
-       assert_eq!(is_vocal(&b'a'), true); 
+    fn detects_vowel() {
+       assert_eq!(is_vowel(&b'a'), true); 
     }
 
     #[test]
-    fn detects_nonvocal() {
-	assert_eq!(is_vocal(&b'n'), false);
+    fn detects_nonvowel() {
+	assert_eq!(is_vowel(&b'n'), false);
     }
 
     #[test]
