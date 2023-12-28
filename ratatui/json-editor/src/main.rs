8928481904a5059af03267b55fb9d785cbd70386
Haf <haf@protonmail.ch>
2023-12-28 05:37:24 +0100

    added centered rect before I added main

--- ratatui/json-editor/src/main.rs
+++ ratatui/json-editor/src/main.rs
@@ -1,4 +1,5 @@
 mod app;
+mod ui;
 
 fn main() {
     println!("Hello, world!");
---
+++ ratatui/json-editor/src/ui.rs
@@ -0,0 +1,26 @@
+use ratatui::layout::{Rect, Layout, Direction, Constraint};
+
+/// helper function to create a centered rect using up certain percentage of the availavble rect `r`
+fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
+    // Cut the given rectangle into three vertical pieces
+    let popup_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+	    Constraint::Percentage((100 - percent_y) / 2),
+	    Constraint::Percentage(percent_y),
+	    Constraint::Percentage((100 - percent_y) / 2),
+	])
+        .split(r);
+
+    // Then cut the middle vertical piece into three width-wise pieces
+    Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+	    Constraint::Percentage((100 - percent_x) / 2),
+	    Constraint::Percentage(percent_x),
+	    Constraint::Percentage((100 - percent_x) / 2),
+	])
+        .split(popup_layout[1])[1] // Return the middle chunk
+}
+
+
