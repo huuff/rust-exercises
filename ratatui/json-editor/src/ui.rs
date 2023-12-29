6b5af449c96565c1b740612d562fadae355a8e47
Haf <haf@protonmail.ch>
2023-12-29 06:36:39 +0100

    small refactor

--- ratatui/json-editor/src/ui.rs
+++ ratatui/json-editor/src/ui.rs
@@ -1,8 +1,8 @@
 use ratatui::{
     layout::{Constraint, Direction, Layout, Rect},
     style::{Color, Style},
-    text::{Text, Line, Span},
-    widgets::{Block, Borders, Paragraph, ListItem, List},
+    text::{Line, Span, Text},
+    widgets::{Block, Borders, List, ListItem, Paragraph},
     Frame,
 };
 
@@ -31,57 +31,42 @@ pub fn ui(f: &mut Frame, app: &App) {
     f.render_widget(title, chunks[0]);
 
     // TODO: Refactor to a .map
-    // TODO: Most of the below's code is for rendring the footer, we could extract it
-    // to a different function
     let mut list_items = Vec::<ListItem>::new();
 
     for key in app.pairs.keys() {
-	list_items.push(ListItem::new(Line::from(Span::styled(
-	    format!("{: <25}: {}", key, app.pairs[key]),
-	    Style::default().fg(Color::Yellow),
-	))))
+        list_items.push(ListItem::new(Line::from(Span::styled(
+            format!("{: <25}: {}", key, app.pairs[key]),
+            Style::default().fg(Color::Yellow),
+        ))))
     }
 
     let list = List::new(list_items);
 
     f.render_widget(list, chunks[1]);
 
+    // TODO: Most of the below's code is for rendring the footer, we could extract it
+    // to a different function
     let current_navigation_text = vec![
-	// The first half of the text
-	match app.current_screen {
-	    CurrentScreen::Main => {
-		Span::styled("Normal Mode", Style::default().fg(Color::Green))
-	    }
-	    CurrentScreen::Editing => {
-		Span::styled("Editing Mode", Style::default().fg(Color::Yellow))
-	    }
-	    CurrentScreen::Exiting => {
-		Span::styled("Exiting", Style::default().fg(Color::LightRed))
-	    }
-	},
-	// A white divider bar to separate the two sections
-	Span::styled(" | ", Style::default().fg(Color::White)),
-	// The final section of the text, which hints on what the user is editing
-	{
-	    // TODO: Simplify to a single match
-	    if let Some(editing) = &app.currently_editing {
-		match editing {
-		    CurrentlyEditing::Key => Span::styled(
-			"Editing Json Key",
-			Style::default().fg(Color::Green)
-		    ),
-		    CurrentlyEditing::Value => Span::styled(
-			"Editing Json Value",
-			Style::default().fg(Color::LightGreen),
-		    ),
-		}
-	    } else {
-		Span::styled(
-		    "Not editing Anything",
-		    Style::default().fg(Color::DarkGray)
-		)
-	    }
-	}
+        // The first half of the text
+        match app.current_screen {
+            CurrentScreen::Main => Span::styled("Normal Mode", Style::default().fg(Color::Green)),
+            CurrentScreen::Editing => {
+                Span::styled("Editing Mode", Style::default().fg(Color::Yellow))
+            }
+            CurrentScreen::Exiting => Span::styled("Exiting", Style::default().fg(Color::LightRed)),
+        },
+        // A white divider bar to separate the two sections
+        Span::styled(" | ", Style::default().fg(Color::White)),
+        // The final section of the text, which hints on what the user is editing
+        match &app.currently_editing {
+            Some(CurrentlyEditing::Key) => {
+                Span::styled("Editing Json Key", Style::default().fg(Color::Green))
+            }
+            Some(CurrentlyEditing::Value) => {
+                Span::styled("Editing Json Value", Style::default().fg(Color::LightGreen))
+            }
+            None => Span::styled("Not editing Anything", Style::default().fg(Color::DarkGray)),
+        },
     ];
 
     let mode_footer = Paragraph::new(Line::from(current_navigation_text))
@@ -89,21 +74,21 @@ pub fn ui(f: &mut Frame, app: &App) {
 
     let current_keys_hint = match app.current_screen {
         CurrentScreen::Main => Span::styled(
-	    "(q) to quit / (e) to make new pair",
-	    Style::default().fg(Color::Red),
-	),
+            "(q) to quit / (e) to make new pair",
+            Style::default().fg(Color::Red),
+        ),
         CurrentScreen::Editing => Span::styled(
-	    "(ESC) to cancel/(Tab) to switch boxes/(enter) to complete",
-	    Style::default().fg(Color::Red),
-	),
+            "(ESC) to cancel/(Tab) to switch boxes/(enter) to complete",
+            Style::default().fg(Color::Red),
+        ),
         CurrentScreen::Exiting => Span::styled(
-	    "(q) to quit / (e) to make new pair",
-	    Style::default().fg(Color::Red),
-	)
+            "(q) to quit / (e) to make new pair",
+            Style::default().fg(Color::Red),
+        ),
     };
 
-    let key_notes_footer = Paragraph::new(Line::from(current_keys_hint))
-        .block(Block::default().borders(Borders::ALL));
+    let key_notes_footer =
+        Paragraph::new(Line::from(current_keys_hint)).block(Block::default().borders(Borders::ALL));
 
     let footer_chunks = Layout::default()
         .direction(Direction::Horizontal)
