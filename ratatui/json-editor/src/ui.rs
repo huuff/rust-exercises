8fe2c2e1f20397b3d2530dc677070022158a456d
Haf <haf@protonmail.ch>
2023-12-30 14:02:50 +0100

    editing popup

--- ratatui/json-editor/src/ui.rs
+++ ratatui/json-editor/src/ui.rs
@@ -23,6 +23,42 @@ pub fn ui(f: &mut Frame, app: &App) {
     render_body(f, app, main_layout[1]);
 
     render_footer(f, &app, main_layout[2]);
+
+    if let Some(editing) = &app.currently_editing {
+	let popup_block = Block::default()
+	    .title("Enter a new key-value pair")
+	    .borders(Borders::NONE)
+	    .style(Style::default().bg(Color::DarkGray));
+	
+	let area = centered_rect(60, 25, f.size());
+	f.render_widget(popup_block, area);
+
+	let popup_chunks = Layout::default()
+	    .direction(Direction::Horizontal)
+	    .margin(1)
+	    .constraints([
+		Constraint::Percentage(50),
+		Constraint::Percentage(50),
+	    ])
+	    .split(area);
+
+	let mut key_block = Block::default().title("Key").borders(Borders::ALL);
+	let mut value_block = Block::default().title("Value").borders(Borders::ALL);
+
+	let active_style = Style::default().bg(Color::LightYellow).fg(Color::Black);
+
+	match editing {
+	    CurrentlyEditing::Key => key_block = key_block.style(active_style),
+	    CurrentlyEditing::Value => value_block = value_block.style(active_style),
+	}
+
+	let key_text = Paragraph::new(app.key_input.clone()).block(key_block);
+	f.render_widget(key_text, popup_chunks[0]);
+
+	let value_text = Paragraph::new(app.value_input.clone()).block(value_block);
+	f.render_widget(value_text, popup_chunks[1]);
+	
+    }
 }
 
 fn render_header(f: &mut Frame, app: &App, target_area: Rect) {
