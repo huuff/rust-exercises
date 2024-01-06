b85dff68fcf9809a369ab8972ffd21408fe28dbc
Haf <haf@protonmail.ch>
2024-01-06 22:09:10 +0100

    dioxus web example

---
+++ dioxus/web/src/main.rs
@@ -0,0 +1,21 @@
+#![allow(non_snake_case)]
+// import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
+use dioxus::prelude::*;
+
+fn main() {
+    // launch the web app
+    dioxus_web::launch(App);
+}
+
+// create a component that renders a div with the text "Hello, world!"
+fn App(cx: Scope) -> Element {
+    let mut count = use_state(cx, || 0);
+    
+    cx.render(rsx! {
+	h1 { "High-Five counter: {count}" }
+        button { onclick: move |_| count += 1, "Up high!" }
+        button { onclick: move |_| count -= 1, "Down low!" }
+    })
+}
+
+
--- flake.nix
+++ flake.nix
@@ -3,12 +3,16 @@
     naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
+  outputs = { self, nixpkgs, utils, naersk, rust-overlay }:
     utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { 
+          inherit system; 
+          overlays = [ rust-overlay.overlay ];
+        };
         naersk-lib = pkgs.callPackage naersk { };
       in
       {
@@ -16,11 +20,17 @@
         devShell = with pkgs; mkShell {
           buildInputs = [
             cargo 
-            rustc 
+            (rust-bin.stable.latest.default.override {
+              targets = [ "wasm32-unknown-unknown" ];
+            })
             rustfmt 
             pre-commit 
             rustPackages.clippy 
             rust-analyzer
+            rustup
+            dioxus-cli
+            wasm-pack
+            wasm-bindgen-cli
           ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
