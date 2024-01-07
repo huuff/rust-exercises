33e5d040ef8eed6c82f4a489d43be4a0072e1431
Haf <haf@protonmail.ch>
2024-01-07 11:26:19 +0100

    moving through the table

--- rust-book/employees/src/main.rs
+++ rust-book/employees/src/main.rs
@@ -4,6 +4,7 @@ mod event;
 use std::collections::HashMap;
 use crossterm::event::KeyCode;
 use event::{EventHandler, Event};
+use ratatui::widgets::TableState;
 use strum::{EnumIter, IntoStaticStr};
 
 
@@ -20,9 +21,18 @@ pub struct Employee {
     name: String,
 }
 
-#[derive(Default)]
 pub struct App {
-    department_to_employees: HashMap<Department, Vec<Employee>>
+    department_to_employees: HashMap<Department, Vec<Employee>>,
+    table_state: TableState,
+}
+
+impl App {
+    pub fn new() -> Self {
+	Self {
+	    department_to_employees: HashMap::new(),
+	    table_state: TableState::default().with_selected(Some(0)),
+ 	}
+    }
 }
 
 fn create_initial_employees() -> Vec<Employee> {
@@ -33,7 +43,7 @@ fn create_initial_employees() -> Vec<Employee> {
 }
 
 fn main() -> anyhow::Result<()> {
-    let mut app = App::default();
+    let mut app = App::new();
     app.department_to_employees.insert(Department::None, create_initial_employees());
     let event_handler = EventHandler::new(16);
 
@@ -42,11 +52,13 @@ fn main() -> anyhow::Result<()> {
     loop {
 	match event_handler.next()? {
 	    Event::Tick => {
-		terminal.draw(|frame| ui::render(frame, &app))?;
+		terminal.draw(|frame| ui::render(frame, &mut app))?;
 	    },
 	    Event::Key(key) => {
 		match key.code {
 		    KeyCode::Char('q') => { break; }
+		    KeyCode::Down => { app.table_state.next() }
+		    KeyCode::Up => { app.table_state.previous() }
 		    _ => {}
 		}
 	    }
@@ -56,5 +68,27 @@ fn main() -> anyhow::Result<()> {
 
     ui::close_terminal()?;
     Ok(())
+}
+
+// TODO: Extract this somewhere else
+trait Stepper {
+    fn next(&mut self);
+    fn previous(&mut self);
+}
+
+impl Stepper for TableState {
+    fn next(&mut self) {
+        self.select(match self.selected() {
+            Some(selected) => Some(selected + 1),
+            None => Some(0),
+        })
+    }
+
+    fn previous(&mut self) {
+        self.select(match self.selected() {
+            Some(selected) => Some(selected - 1),
+            None => Some(0),
+        })
+    }
 }
 
--- rust-book/employees/src/ui.rs
+++ rust-book/employees/src/ui.rs
@@ -31,7 +31,7 @@ pub fn close_terminal() -> anyhow::Result<()> {
     Ok(())
 }
 
-pub fn render(f: &mut Frame, app: &App) {
+pub fn render(f: &mut Frame, app: &mut App) {
     let outer_block = Block::default().title("Employees").borders(Borders::ALL);
     f.render_widget(outer_block, f.size());
 
@@ -45,7 +45,7 @@ pub fn render(f: &mut Frame, app: &App) {
     render_derpartment_table(f, app, center_rect);
 }
 
-pub fn render_derpartment_table(f: &mut Frame, app: &App, target_area: Rect) {
+pub fn render_derpartment_table(f: &mut Frame, app: &mut App, target_area: Rect) {
     let widths = [Constraint::default(), Constraint::Length(10)];
     // TODO: It'd be cool if I could use some &strs here
     let rows = Department::iter()
@@ -67,7 +67,8 @@ pub fn render_derpartment_table(f: &mut Frame, app: &App, target_area: Rect) {
                 .add_modifier(Modifier::UNDERLINED),
         )
         .block(Block::default().borders(Borders::ALL))
+        .highlight_style(Style::new().on_dark_gray())
         .segment_size(SegmentSize::EvenDistribution);
 
-    f.render_widget(table, target_area);
+    f.render_stateful_widget(table, target_area, &mut app.table_state);
 }
