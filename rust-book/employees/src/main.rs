8df66c866885e19d549c40bfbdca28f161a6532b
Haf <haf@protonmail.ch>
2024-01-16 20:36:25 +0100

    some refactors

--- rust-book/employees/src/main.rs
+++ rust-book/employees/src/main.rs
@@ -1,9 +1,9 @@
-mod event;
 mod data;
-mod scene;
-mod ui;
+mod event;
 mod models;
+mod scene;
 mod types;
+mod ui;
 mod util;
 
 use std::collections::BTreeSet;
@@ -16,7 +16,6 @@ use scene::Scene;
 use types::DepartmentToEmployeeMap;
 use util::extract;
 
-
 pub struct App {
     department_to_employees: DepartmentToEmployeeMap,
     selected_employee: Option<Employee>,
@@ -25,18 +24,18 @@ pub struct App {
 impl App {
     pub fn new(initial_staff: DepartmentToEmployeeMap) -> Self {
         Self {
-	    department_to_employees: initial_staff,
-	    selected_employee: None,
+            department_to_employees: initial_staff,
+            selected_employee: None,
         }
     }
 
-    pub fn num_departments(&self) -> usize {
-	self.department_to_employees.len()
+    pub fn add_employee(&mut self, department: &Department, employee: Employee) {
+	self.department_to_employees
+	    .get_mut(department)
+	    .unwrap()
+	    .insert(employee);
     }
 
-    pub fn num_employees(&self, department: &Department) -> usize {
-	self.department_to_employees[department].len()
-    }
 }
 
 // TODO: Some keybinding to cancel the employee move
@@ -52,43 +51,51 @@ fn main() -> anyhow::Result<()> {
             Event::Tick => {
                 terminal.draw(|frame| ui::render(frame, &mut scene, &app))?;
             }
-            Event::Key(key) => {
-                match key.code {
-                    KeyCode::Char('q') => break,
-                    KeyCode::Down => scene.next(),
-                    KeyCode::Up => scene.previous(),
-                    KeyCode::Enter => {
-                        scene = match scene {
-                            Scene::DepartmentList { state, .. } => {
-				// TODO: Maybe avoid the news?
-                                if let Some(selected) = state.selected() {
-                                    let department = *app.department_to_employees.keys().nth(selected).unwrap();
-				    if let Some(selected_employee) = app.selected_employee.take() {
-					app.department_to_employees.get_mut(&department).unwrap().insert(selected_employee);
-					Scene::new_department_list(&app.department_to_employees)
-				    } else {
-					Scene::new_department_view(department, &app.department_to_employees[&department])
-				    }
-                                } else {
-				    Scene::new_department_list(&app.department_to_employees)
-				}
-                            }
-                            Scene::DepartmentView { department, state, ..  } => {
-				let employees = app.department_to_employees.remove(&department).unwrap();
-				let (employee, employees) = extract(employees.into_iter()
-								    .enumerate(),
-								    |(i, _)| state.selected().is_some_and(|selected| selected == *i) )
-				    ;
-				let employees = employees.into_iter().map(|it| it.1).collect::<BTreeSet<Employee>>();
-				app.department_to_employees.insert(department, employees);
-				app.selected_employee = employee.map(|it| it.1);
-				Scene::new_department_list(&app.department_to_employees)
-			    }
-                        };
-                    }
-                    _ => {}
+            Event::Key(key) => match key.code {
+                KeyCode::Char('q') => break,
+                KeyCode::Down => scene.next(),
+                KeyCode::Up => scene.previous(),
+                KeyCode::Enter => {
+                    match scene {
+                        Scene::DepartmentList { ref state, .. } => {
+                            if let None = state.selected() {
+                                continue;
+                            };
+
+                            let selected = state.selected().unwrap();
+                            let department =
+                                *app.department_to_employees.keys().nth(selected).unwrap();
+                            scene = if let Some(selected_employee) = app.selected_employee.take() {
+				app.add_employee(&department, selected_employee);
+                                Scene::new_department_list(&app.department_to_employees)
+                            } else {
+                                Scene::new_department_view(
+                                    department,
+                                    &app.department_to_employees[&department],
+                                )
+                            };
+                        }
+                        Scene::DepartmentView {
+                            department, state, ..
+                        } => {
+                            let employees =
+                                app.department_to_employees.remove(&department).unwrap();
+                            let (employee, employees) =
+                                extract(employees.into_iter().enumerate(), |(i, _)| {
+                                    state.selected().is_some_and(|selected| selected == *i)
+                                });
+                            let employees = employees
+                                .into_iter()
+                                .map(|it| it.1)
+                                .collect::<BTreeSet<Employee>>();
+                            app.department_to_employees.insert(department, employees);
+                            app.selected_employee = employee.map(|it| it.1);
+                            scene = Scene::new_department_list(&app.department_to_employees);
+                        }
+                    };
                 }
-            }
+                _ => {}
+            },
             _ => {}
         }
     }
