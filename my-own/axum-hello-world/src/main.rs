2c2e8f6b62c3dc9f0f19597d588e5f01697d621a
Haf <haf@protonmail.ch>
2024-01-20 13:54:58 +0100

    extracting to a function

--- my-own/axum-hello-world/src/main.rs
+++ my-own/axum-hello-world/src/main.rs
@@ -2,7 +2,7 @@
 extern crate rust_i18n;
 extern crate accept_language;
 
-use axum::{Router, routing::get, extract::Query, response::IntoResponse, http::{HeaderMap, header}};
+use axum::{Router, routing::get, extract::Query, response::IntoResponse, http::{HeaderMap, header, HeaderValue}};
 use serde::Deserialize;
 use rust_i18n::t;
 
@@ -23,10 +23,16 @@ struct InputQuery {
     name: Option<String>,
 }
 
+// TODO: Maybe some logging
 async fn hello_world(Query(query): Query<InputQuery>, headers: HeaderMap) -> impl IntoResponse {
-    let request_language = headers.get(header::ACCEPT_LANGUAGE)
-        // TODO: Error handling
-        .map(|it| it.to_str().unwrap());
+    let request_language = headers.get(header::ACCEPT_LANGUAGE);
+
+    get_hello_string(query.name, request_language)
+}
+
+
+fn get_hello_string(name: Option<String>, request_language: Option<&HeaderValue>) -> String {
+    let request_language = request_language.map(|it| it.to_str().unwrap());
     let language = request_language
         .map(|request_language| accept_language::intersection(request_language, &rust_i18n::available_locales!()[..]))
         .and_then(|common_languages| common_languages.into_iter().next())
@@ -34,7 +40,8 @@ async fn hello_world(Query(query): Query<InputQuery>, headers: HeaderMap) -> imp
         .unwrap_or(rust_i18n::locale().to_string())
         ;
 
-    let name = query.name.as_deref().map(|it| Cow::from(it)).unwrap_or(t!("world", locale = &language));
+    // TODO: Can I avoid to_string?
+    let name = name.unwrap_or(t!("world", locale = &language).to_string());
 
-    t!("hello_name", locale = &language, name = name)
+    t!("hello_name", locale = &language, name = name).to_string()
 }
